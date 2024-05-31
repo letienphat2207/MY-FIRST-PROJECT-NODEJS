807a9af6f77761d70d43810d033a018890b9eeae
letienphat2207 <letienphat2207@gmail.com>
2024-05-31 21:08:59 +0700

    video 19

--- src/server.js
+++ src/server.js
@@ -1,21 +1,32 @@
 const express = require('express') //import express
+const path = require('path');
+require('dotenv').config()
+
 const app = express() // tạo express application
-const port = 8080 // init port
+const port = process.env.PORT||8082 // init port
+const hostname  = process.env.HOST_NAME
+//config template engine 
+//app.set('views', './views')
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+
+
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 app.get('/', (req, res) => {
-   res.send('Hello World!')
+        res.send('Hello World!')
 })
 
 app.get('/abc', (req, res) => {
-    res.send('check ABC')
+        res.send('check ABC')
  })
 
  app.get('/hoidanit', (req, res) => {
-    res.send('<h1>Hoi dan it </h1>')
+       res.render('./sample.ejs')
  })
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
-app.listen(port, () => {
+app.listen(port, hostname , () => {
    console.log(`Example app listening on port ${port}`)
 })
